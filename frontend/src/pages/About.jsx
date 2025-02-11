eb3f73c03da13781d45dcbbd465267c68f1bc917
mohamedali069 <mohamedali069@gmail.com>
2025-02-11 02:35:00 +0200

    this all frontend code commit

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewletterBox from '../components/NewletterBox'
 
 const About = () => {
   return (
     <div>
-      
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={'ABOUT'} text2={'US'} />
+      </div>
+      <div className="flex my-10 flex-col sm:flex-row gap-16">
+        <img className='w-full md:max-w-[450px] ' src={assets.about_img} alt="about-image" />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero veritatis numquam, fugiat odio consequatur harum adipisci minus officiis quisquam officia explicabo illum temporibus vitae architecto in error natus quod hic.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, obcaecati ex? Expedita, voluptas nam alias inventore laboriosam deserunt illum est? Ea cum explicabo inventore fuga deleniti fugit voluptatibus, natus cumque?</p>
+        
+        <b className='text-gray-800'>Our Mission </b>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam omnis mollitia consequuntur dicta. Sit quo perferendis quasi temporibus illo adipisci vel, rerum veritatis asperiores non ea fugit debitis voluptate blanditiis!</p>
+        </div>
+      </div>
+      <div className="text-xl py-4">
+        <Title text1={'WHY'} text2={' CHOOSE US'} />
+      </div>
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b> Qaulity Assurance</b>
+          <p className='text-gray-700'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium, eaque nam? Explicabo voluptates officiis esse aliquid repellendus sint ipsam accusantium quas vero accusamus exercitationem nostrum hic, officia perferendis ab rerum.</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b> Qaulity Assurance</b>
+          <p className='text-gray-700'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium, eaque nam? Explicabo voluptates officiis esse aliquid repellendus sint ipsam accusantium quas vero accusamus exercitationem nostrum hic, officia perferendis ab rerum.</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b> Qaulity Assurance</b>
+          <p className='text-gray-700'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium, eaque nam? Explicabo voluptates officiis esse aliquid repellendus sint ipsam accusantium quas vero accusamus exercitationem nostrum hic, officia perferendis ab rerum.</p>
+        </div>
+      </div>
+      <NewletterBox />
     </div>
   )
 }
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewletterBox from '../components/NewletterBox'
 
 const Contact = () => {
   return (
     <div>
-      
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={'CONTACT'} text2={'US'} />
+      </div>
+      <div className="flex my-10 flex-col sm:flex-row mb-28 gap-16">
+              <img className='w-full md:max-w-[480px] ' src={assets.contact_img} alt="about-image" />
+              <div className="flex flex-col justify-center items-start gap-6">
+                <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+                <p className='text-gray-500'>54789 willms station <br /> suite350,washington ,USA</p>
+                <p className='text-gray-500'>Tel: (552)555-6555 <br />Email: admin@forever.com</p>
+                <p className='font-semibold text-xl text-gray-600'>Careers at forever</p>
+                <p className='text-gray-500' > learn more about our teams and job openings. </p>
+                <button className='cursor-pointer border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+              </div>
+      </div> 
+      <NewletterBox/>       
     </div>
   )
 }
